776ab2e591ae0e3102660f982e1e6d091c08295e
Joe Flateau <joe@joeflateau.net>
2022-09-23 13:30:09 -0400

    add button that should open a browser

--- www/js/capacitor-welcome.js
+++ www/js/capacitor-welcome.js
@@ -1,12 +1,12 @@
 window.customElements.define(
-  'capacitor-welcome',
+  "capacitor-welcome",
   class extends HTMLElement {
     constructor() {
       super();
 
       Capacitor.Plugins.SplashScreen.hide();
 
-      const root = this.attachShadow({ mode: 'open' });
+      const root = this.attachShadow({ mode: "open" });
 
       root.innerHTML = `
     <style>
@@ -58,28 +58,7 @@ window.customElements.define(
       </capacitor-welcome-titlebar>
       <main>
         <p>
-          Capacitor makes it easy to build powerful apps for the app stores, mobile web (Progressive Web Apps), and desktop, all
-          with a single code base.
-        </p>
-        <h2>Getting Started</h2>
-        <p>
-          You'll probably need a UI framework to build a full-featured app. Might we recommend
-          <a target="_blank" href="http://ionicframework.com/">Ionic</a>?
-        </p>
-        <p>
-          Visit <a href="http://ionic-team.github.io/capacitor">ionic-team.github.io/capacitor</a> for information
-          on using native features, building plugins, and more.
-        </p>
-        <a href="http://ionic-team.github.io/capacitor" target="_blank" class="button">Read more</a>
-        <h2>Tiny Demo</h2>
-        <p>
-          This demo shows how to call Capacitor plugins. Say cheese!
-        </p>
-        <p>
-          <button class="button" id="take-photo">Take Photo</button>
-        </p>
-        <p>
-          <img id="image" style="max-width: 100%">
+          <button class="button" id="open-browser">Open Browser</button>
         </p>
       </main>
     </div>
@@ -90,35 +69,28 @@ window.customElements.define(
       const self = this;
 
       self.shadowRoot
-        .querySelector('#take-photo')
-        .addEventListener('click', async function (e) {
-          const { Camera } = Capacitor.Plugins;
+        .querySelector("#open-browser")
+        .addEventListener("click", async function (e) {
+          const { Browser } = Capacitor.Plugins;
 
           try {
-            const photo = await Camera.getPhoto({
-              resultType: 'uri',
+            await Browser.open({
+              url: "http://ionic-team.github.io/capacitor",
             });
-
-            const image = self.shadowRoot.querySelector('#image');
-            if (!image) {
-              return;
-            }
-
-            image.src = photo.webPath;
           } catch (e) {
-            console.warn('User cancelled', e);
+            console.warn("User cancelled", e);
           }
         });
     }
-  },
+  }
 );
 
 window.customElements.define(
-  'capacitor-welcome-titlebar',
+  "capacitor-welcome-titlebar",
   class extends HTMLElement {
     constructor() {
       super();
-      const root = this.attachShadow({ mode: 'open' });
+      const root = this.attachShadow({ mode: "open" });
       root.innerHTML = `
     <style>
       :host {
@@ -139,5 +111,5 @@ window.customElements.define(
     <slot></slot>
     `;
     }
-  },
+  }
 );
